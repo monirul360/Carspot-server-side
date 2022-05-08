5cb0707efd2bf212520f27f3808404370ae9740c
monirul360 <monirulislam65361@gmail.com>
2022-05-08 14:52:40 +0600

    items api create

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.USNAME}:${process.env.US_PASSWORD}@cluster0.gmbsx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log('Mongodb connect');
-client.connect(err => {
-    const collection = client.db("carspot").collection("items");
-    // perform actions on the collection object
-    client.close();
-});
 
+const uri = `mongodb+srv://carspot1:HZFw5jWHcVhWTm6x@cluster0.mqftf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const Carscollection = client.db("carspot1").collection("items");
+        app.get('/items', async (req, res) => {
+            const query = {}
+            const cursor = Carscollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Server running!')
 })
