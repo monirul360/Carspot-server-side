d38f9a6ee4a242beea1e19407e1232d421e4e4a8
monirul360 <monirulislam65361@gmail.com>
2022-05-08 13:59:48 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USNAME}:${process.env.US_PASSWORD}@cluster0.gmbsx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('Mongodb connect');
+client.connect(err => {
+    const collection = client.db("carspot").collection("items");
+    // perform actions on the collection object
+    client.close();
+});
+
 app.get('/', (req, res) => {
     res.send('Server running!')
 })
