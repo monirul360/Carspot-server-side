2c85dc02c56ab7814392891efdabed21fad1321b
monirul360 <monirulislam65361@gmail.com>
2022-05-08 18:06:46 +0600

    items query show

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
@@ -20,6 +20,12 @@ async function run() {
             const query = {}
             const cursor = Carscollection.find(query);
             const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get("/items/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await Carscollection.findOne(query);
             res.send(result);
         })
     }
